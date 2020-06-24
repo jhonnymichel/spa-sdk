802955c575f16d6075f5856de53341fc22a690ab
Jhonny Michel <jhonnymichel@outlook.com>
2020-06-23 22:39:50 -0300

    testing the reload button

--- tests/pages/todos.test.tsx
+++ tests/pages/todos.test.tsx
@@ -3,6 +3,7 @@ import { rest } from 'msw'
 import server from 'tests/server'
 import { resetStore, initializeStore } from 'store'
 import { render, screen, waitForElementToBeRemoved } from 'tests/utils'
+import { fireEvent } from '@testing-library/react'
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
@@ -26,6 +27,16 @@ describe('Todos page', () => {
     render(<TodosPage />)
     await waitForElementToBeRemoved(() => screen.queryByText('Loading todos'))
     expect(screen.queryAllByText('test todo')).toHaveLength(2)
+  })
+
+  it('Restart the cycle when the Reload button is clicked', async () => {
+    store.todos.fetch()
+    render(<TodosPage />)
+    await waitForElementToBeRemoved(() => screen.queryByText('Loading todos'))
+    expect(screen.queryAllByText('test todo')).toHaveLength(2)
+
+    fireEvent.click(screen.getByText('Reload'))
+    expect(screen.getByText('Loading todos')).toBeTruthy()
   })
 
   it('Displays an error message if load fails', async () => {
